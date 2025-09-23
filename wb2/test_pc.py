25ccd6166e9991d5419c8599744bea2285ef1d01
tobiasbiegert <tobias-biegert@web.de>
2025-09-23 14:48:53 +0200

    Update p-value computation and faster matvec

--- wb2/test_pc.py
+++ wb2/test_pc.py
@@ -18,7 +18,7 @@ def block_permutation_test(
         score_a, score_b (1-D array-like): Score (here CRPS) values from model A and model B over initialization times
         seed_offset (int): Unique offset for random seed per grid point & lead time
         block_length (int): Number of consecutive init-time steps per block (here 2, 6, 10, 14, 20)
-        b (int): Number of bootstrap replicates
+        b (int): Number of permutation replicates
         root_seed (int): Global RNG seed
 
     Returns:
@@ -36,7 +36,7 @@ def block_permutation_test(
 
     # draw a b×n_blocks matrix of +/- signs with a deterministic seed
     rng = np.random.default_rng(root_seed + int(seed_offset))
-    block_signs = rng.choice((-1, 1), size=(b, n_blocks), replace=True)
+    block_signs = (1 - 2 * rng.integers(0, 2, size=(b, n_blocks), dtype=np.int8)) # {+1,-1}
 
     # Offsets to slide block boundaries across replicates
     offsets = np.arange(b, dtype=int) % block_length
@@ -47,13 +47,16 @@ def block_permutation_test(
     # Expand block signs to full-series signs per replicate
     signs = block_signs[np.arange(b)[:, None], block_indices]
 
-    # Compute bootstrap means
-    m = (signs * d[None, :]).mean(axis=1)
-
-    # empirical one‑sided p‑value
-    p = np.mean(m <= d_mean)
+    # Compute replicate means
+    m = np.einsum('ij,j->i', signs, d) / n # signs promoted to float on the fly
     
-    return p.astype(np.float32)
+    # one‑sided p‑value
+    n_lt = np.count_nonzero(m < d_mean)
+    n_gt = np.count_nonzero(m > d_mean)
+    n_eq = b - n_lt - n_gt                    # compute ties robustly as remainder
+    p = (n_lt + 0.5 * n_eq + 0.5) / (b + 1)
+    
+    return np.float32(p)
 
 def block_permutation_test_dataset(
     ds_a: xr.Dataset,                 
@@ -68,7 +71,7 @@ def block_permutation_test_dataset(
     
     Parameters:
         ds_a, ds_b (xr.Dataset): Score (here CRPS) values from model A and model B
-        b (int): Number of bootstrap replicates
+        b (int): Number of permutation replicates
         seed (int): global RNG seed
         variables (list[str], optional): which data_vars to test
         
@@ -139,70 +142,74 @@ variables = [
     'total_precipitation_24hr',
 ]
 # Define chunking scheme
-chunking_dict = {'longitude':8}
+chunking_dict = {'longitude': 60, 'latitude': 11, 'prediction_timedelta': 1, 'time': -1} # {'longitude':8}
 
-# Open CRPS data sets
-graphcast_vs_era5_crps = xr.open_dataset('results/graphcast_vs_era5_crps.nc', decode_timedelta=True).sel(prediction_timedelta=lead_times).chunk(chunking_dict)
-pangu_vs_era5_crps = xr.open_dataset('results/pangu_vs_era5_crps.nc', decode_timedelta=True).sel(prediction_timedelta=lead_times).chunk(chunking_dict)
-ifs_hres_vs_era5_crps = xr.open_dataset('results/ifs_hres_vs_era5_crps.nc', decode_timedelta=True).sel(prediction_timedelta=lead_times).chunk(chunking_dict)
+def main():
+    # Open CRPS data sets
+    graphcast_vs_era5_crps = xr.open_dataset('results/graphcast_vs_era5_crps.nc', decode_timedelta=True).sel(prediction_timedelta=lead_times).chunk(chunking_dict)
+    pangu_vs_era5_crps = xr.open_dataset('results/pangu_vs_era5_crps.nc', decode_timedelta=True).sel(prediction_timedelta=lead_times).chunk(chunking_dict)
+    ifs_hres_vs_era5_crps = xr.open_dataset('results/ifs_hres_vs_era5_crps.nc', decode_timedelta=True).sel(prediction_timedelta=lead_times).chunk(chunking_dict)
+    
+    graphcast_vs_ifs_analysis_crps = xr.open_dataset('results/graphcast_vs_ifs_analysis_crps.nc', decode_timedelta=True).sel(prediction_timedelta=lead_times).chunk(chunking_dict)
+    pangu_vs_ifs_analysis_crps = xr.open_dataset('results/pangu_vs_ifs_analysis_crps.nc', decode_timedelta=True).sel(prediction_timedelta=lead_times).chunk(chunking_dict)
+    ifs_hres_vs_ifs_analysis_crps = xr.open_dataset('results/ifs_hres_vs_ifs_analysis_crps.nc', decode_timedelta=True).sel(prediction_timedelta=lead_times).chunk(chunking_dict)
+    
+    # Run model-pair comparisons
+    # precipitation is omitted if one of the models (depending on reference) does not include it
+    
+    # GraphCast vs Pangu (ERA5 ground truth)
+    with ProgressBar():
+        graphcast_pangu_vs_era5_p = block_permutation_test_dataset(
+            graphcast_vs_era5_crps, 
+            pangu_vs_era5_crps,
+            variables=variables[:-1]
+            ).compute()
+    graphcast_pangu_vs_era5_p.to_netcdf('results/graphcast_pangu_vs_era5_p.nc')
+    
+    # GraphCast vs Pangu (IFS‑Analysis ground truth)
+    with ProgressBar():
+        graphcast_pangu_vs_ifs_analysis_p = block_permutation_test_dataset(
+            graphcast_vs_ifs_analysis_crps, 
+            pangu_vs_ifs_analysis_crps,
+            variables=variables[:-1]
+            ).compute()
+    graphcast_pangu_vs_ifs_analysis_p.to_netcdf('results/graphcast_pangu_vs_ifs_analysis_p.nc')
+    
+    # GraphCast vs IFS‑HRES (ERA5 ground truth) – only comparison with precipitation
+    with ProgressBar():
+        graphcast_ifs_hres_vs_era5_p = block_permutation_test_dataset(
+            graphcast_vs_era5_crps, 
+            ifs_hres_vs_era5_crps,
+            variables=variables
+            ).compute()
+    graphcast_ifs_hres_vs_era5_p.to_netcdf('results/graphcast_ifs_hres_vs_era5_p.nc')
+    
+    # GraphCast vs IFS‑HRES (IFS‑Analysis ground truth)
+    with ProgressBar():
+        graphcast_ifs_hres_vs_ifs_analysis_p = block_permutation_test_dataset(
+            graphcast_vs_ifs_analysis_crps, 
+            ifs_hres_vs_ifs_analysis_crps,
+            variables=variables[:-1]
+            ).compute()
+    graphcast_ifs_hres_vs_ifs_analysis_p.to_netcdf('results/graphcast_ifs_hres_vs_ifs_analysis_p.nc')
+    
+    # Pangu vs IFS‑HRES (ERA5 ground truth)
+    with ProgressBar():
+        pangu_hres_vs_era5_p = block_permutation_test_dataset(
+            pangu_vs_era5_crps, 
+            ifs_hres_vs_era5_crps,
+            variables=variables[:-1]
+            ).compute()
+    pangu_hres_vs_era5_p.to_netcdf('results/pangu_hres_vs_era5_p.nc')
+    
+    # Pangu vs IFS‑HRES (IFS‑Analysis ground truth)
+    with ProgressBar():    
+        pangu_hres_vs_ifs_analysis_p = block_permutation_test_dataset(
+            pangu_vs_ifs_analysis_crps, 
+            ifs_hres_vs_ifs_analysis_crps,
+            variables=variables[:-1]
+            ).compute()
+    pangu_hres_vs_ifs_analysis_p.to_netcdf('results/pangu_hres_vs_ifs_analysis_p.nc')
 
-graphcast_vs_ifs_analysis_crps = xr.open_dataset('results/graphcast_vs_ifs_analysis_crps.nc', decode_timedelta=True).sel(prediction_timedelta=lead_times).chunk(chunking_dict)
-pangu_vs_ifs_analysis_crps = xr.open_dataset('results/pangu_vs_ifs_analysis_crps.nc', decode_timedelta=True).sel(prediction_timedelta=lead_times).chunk(chunking_dict)
-ifs_hres_vs_ifs_analysis_crps = xr.open_dataset('results/ifs_hres_vs_ifs_analysis_crps.nc', decode_timedelta=True).sel(prediction_timedelta=lead_times).chunk(chunking_dict)
-
-# Run model-pair comparisons
-# precipitation is omitted if one of the models (depending on reference) does not include it
-
-# GraphCast vs Pangu (ERA5 ground truth)
-with ProgressBar():
-    graphcast_pangu_vs_era5_p = block_permutation_test_dataset(
-        graphcast_vs_era5_crps, 
-        pangu_vs_era5_crps,
-        variables=variables[:-1]
-        ).compute()
-graphcast_pangu_vs_era5_p.to_netcdf('results/graphcast_pangu_vs_era5_p.nc')
-
-# GraphCast vs Pangu (IFS‑Analysis ground truth)
-with ProgressBar():
-    graphcast_pangu_vs_ifs_analysis_p = block_permutation_test_dataset(
-        graphcast_vs_ifs_analysis_crps, 
-        pangu_vs_ifs_analysis_crps,
-        variables=variables[:-1]
-        ).compute()
-graphcast_pangu_vs_ifs_analysis_p.to_netcdf('results/graphcast_pangu_vs_ifs_analysis_p.nc')
-
-# GraphCast vs IFS‑HRES (ERA5 ground truth) – only comparison with precipitation
-with ProgressBar():
-    graphcast_ifs_hres_vs_era5_p = block_permutation_test_dataset(
-        graphcast_vs_era5_crps, 
-        ifs_hres_vs_era5_crps,
-        variables=variables
-        ).compute()
-graphcast_ifs_hres_vs_era5_p.to_netcdf('results/graphcast_ifs_hres_vs_era5_p.nc')
-
-# GraphCast vs IFS‑HRES (IFS‑Analysis ground truth)
-with ProgressBar():
-    graphcast_ifs_hres_vs_ifs_analysis_p = block_permutation_test_dataset(
-        graphcast_vs_ifs_analysis_crps, 
-        ifs_hres_vs_ifs_analysis_crps,
-        variables=variables[:-1]
-        ).compute()
-graphcast_ifs_hres_vs_ifs_analysis_p.to_netcdf('results/graphcast_ifs_hres_vs_ifs_analysis_p.nc')
-
-# Pangu vs IFS‑HRES (ERA5 ground truth)
-with ProgressBar():
-    pangu_hres_vs_era5_p = block_permutation_test_dataset(
-        pangu_vs_era5_crps, 
-        ifs_hres_vs_era5_crps,
-        variables=variables[:-1]
-        ).compute()
-pangu_hres_vs_era5_p.to_netcdf('results/pangu_hres_vs_era5_p.nc')
-
-# Pangu vs IFS‑HRES (IFS‑Analysis ground truth)
-with ProgressBar():    
-    pangu_hres_vs_ifs_analysis_p = block_permutation_test_dataset(
-        pangu_vs_ifs_analysis_crps, 
-        ifs_hres_vs_ifs_analysis_crps,
-        variables=variables[:-1]
-        ).compute()
-pangu_hres_vs_ifs_analysis_p.to_netcdf('results/pangu_hres_vs_ifs_analysis_p.nc')
+if __name__ == '__main__':
+    main()
